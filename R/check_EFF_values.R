0c6c8dcffd25378d58131ccb77afd541a210ee25
J. Holden <jeremy.holden@ontario.ca>
2022-01-12 10:45:12 -0500

    check mesh sizes
    EFF is the mesh size field. Checks values in the FN125 table are all part of the standard mesh sizes

---
+++ R/check_EFF_values.R
@@ -0,0 +1,29 @@
+#' check_EFF_values - Checks for unusual/non-standard mesh sizes
+#' @description Most of the gillnets use common sizes. This function checks for sizes not commonly used.
+#' @return
+#' @export
+#'
+
+check_EFF_values <- function(){
+  sqldf::sqldf("SELECT PRJ_CD,
+       SAM,
+       EFF,
+       SPC,
+       GRP,
+       FISH
+FROM FN125
+WHERE (EFF NOT IN ('1',
+                   '2',
+                   '3',
+                   '032',
+                   '038',
+                   '051',
+                   '064',
+                   '076',
+                   '089',
+                   '102',
+                   '114',
+                   '127',
+                   '140',
+                   '153'))")
+}
