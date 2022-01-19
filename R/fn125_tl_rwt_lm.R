17ad98b405526c9616fcb1d7edf0e597ccce00da
J. Holden <jeremy.holden@ontario.ca>
2022-01-19 14:37:57 -0500

    fn125_tl_rwt_lm
    conducts a nls model fit to the flen and rwt data and then uses the log ratio of tlen to predicted tlen to identify potential data errors within the tlen~rwt relationship. optionally returns a plot.

---
+++ R/fn125_tl_rwt_lm.R
@@ -0,0 +1,52 @@
+
+#' tl_rwt_lm - Compares FL:RWT using nlm relationship
+#'
+#' @param FN125
+#' @param makeplot
+#' @param fail_criteria
+#'
+#' @return
+#' @export
+#'
+
+fn125_tl_rwt_lm <- function(FN125, makeplot = F, fail_criteria = 0.3) {
+  if(length(unique(FN125$PRJ_CD)) > 1){usethis::ui_warn("Multiple projects included.")}
+  if(length(unique(FN125$SPC)) > 1){
+    usethis::ui_warn("Multiple species included.")
+    # create custom different plot title
+  }
+  if(length(!(is.na(FN125$RWT))) < 10 | length(!(is.na(FN125$TLEN))) < 10){
+    usethis::ui_oops(paste0("Fewer than 10 records exists for SPC == ", unique(FN125$SPC)))
+    return(FN125)
+  } else {
+    TLEN.RWT <- nls(RWT~alpha*TLEN^(beta), data=FN125, start=list(alpha=0.000005, beta=3))
+    FN125 <- FN125 |>
+      mutate(PRED_TLENRWT = coefficients(TLEN.RWT)[1]*TLEN^coefficients(TLEN.RWT)[2]) |>
+      mutate(LogRatioTLENRWT = abs(log10(PRED_TLENRWT)-log10(RWT))) |>
+      mutate(qid4_error = ifelse(LogRatioTLENRWT > fail_criteria, T, F))
+
+    if(makeplot){
+      require(ggplot2)
+      print(ggplot(FN125, aes(TLEN, RWT, color = qid4_error)) +
+              geom_point() +
+              ggtitle(paste(unique(FN125$PRJ_CD), unique(FN125$SPC), sep = ":")))
+    }
+
+    if(sum(FN125$qid4_error, na.rm = T) == 0){
+      done_message <- paste0("No instances of irregular TLEN~RWT values found for SPC == ", unique(FN125$SPC))
+      usethis::ui_done(done_message)
+    }
+
+    if(sum(FN125$qid4_error, na.rm = T) > 0){
+      n_flags <- sum(FN125$qid4_error, na.rm = T)
+      done_message <- paste0(n_flags,
+                             " instances of irregular TLEN~RWT values found for SPC == ",
+                             unique(FN125$SPC),
+                             "[`FN125 %>% filter(qid3_error)`]")
+      usethis::ui_oops(done_message)
+    }
+    if(is.na(sum(FN125$qid4_error))){usethis::ui_warn("NA values in TLEN or RWT")}
+
+    FN125
+  }
+}
