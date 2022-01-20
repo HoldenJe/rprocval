2f14b6f11f0d134ac26fc3bbe7f137847ccd1170
J. Holden <jeremy.holden@ontario.ca>
2022-01-20 08:04:44 -0500

    fixes plot error when no FLEN/TLEN pairs exist
    fn125_fl_gt_lt crashed when a species list was provided for which there were no paired observations of flen and tlen. Not detected earlier as this test was previously written to skip in nrows < 10 which didn't makes sense for this test since N=1 should still be flagged as an error and doesn't require a model to identify the error.

--- R/fn125_fl_gt_tl.R
+++ R/fn125_fl_gt_tl.R
@@ -8,24 +8,32 @@
 #'
 
 fn125_fl_gt_tl <- function(FN125, makeplot = F){
-
-    if(length(unique(FN125$PRJ_CD)) > 1){usethis::ui_warn("Multiple projects included.")}
-    if(length(unique(FN125$SPC)) > 1){
+  if(length(unique(FN125$PRJ_CD)) > 1){usethis::ui_warn("Multiple projects included.")}
+  if(length(unique(FN125$SPC)) > 1){
       usethis::ui_warn("Multiple species included. This is likely to cause issues with messages and plot titles.")
       # create custom different plot title
     }
-    FN125$qid1_error <- ifelse(FN125$FLEN > FN125$TLEN, T, F)
-    if(makeplot){
+
+  FN125$qid1_error <- ifelse(FN125$FLEN > FN125$TLEN, T, F)
+
+  if(makeplot){
+    if(sum(!is.na(FN125$TLEN)) < 1 | sum(!is.na(FN125$FLEN)) < 1){
+      usethis::ui_oops(paste0("No FLEN TLEN pairs exist for SPC == ", unique(FN125$SPC)))
+      return(FN125)
+    } else {
       require(ggplot2)
       print(ggplot(FN125, aes(FLEN, TLEN, color = qid1_error)) +
-            geom_point() +
-            ggtitle(paste(unique(FN125$PRJ_CD), unique(FN125$SPC), sep=":")))}
-    if(sum(FN125$qid1_error, na.rm = T) == 0){
+            geom_point()  +
+            ggtitle(paste(unique(FN125$PRJ_CD), unique(FN125$SPC), sep=":"))
+      )}
+  }
+  if(sum(FN125$qid1_error, na.rm = T) == 0){
       usethis::ui_done(
         paste0("No instances of FLEN > TLEN found for SPC == ",
                unique(FN125$SPC)))
     }
-    if(sum(FN125$qid1_error, na.rm = T) > 0){
+
+  if(sum(FN125$qid1_error, na.rm = T) > 0){
       n_flags <- sum(FN125$qid1_error, na.rm = T)
       done_message <- paste0(n_flags,
                              " instances FLEN > TLEN found for SPC == ",
@@ -34,5 +42,5 @@ fn125_fl_gt_tl <- function(FN125, makeplot = F){
       usethis::ui_oops(done_message)
     }
 
-    FN125
+  FN125
 }
