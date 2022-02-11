6458413bba0cb76b1d63c83f397e8c59d030444d
J. Holden <jeremy.holden@ontario.ca>
2022-02-11 16:34:29 -0500

    updates to fn121 column check

--- R/fn121_column_check.R
+++ R/fn121_column_check.R
@@ -1,6 +1,6 @@
 #' Check FN121 Columns
 #'
-#' @param FN121
+#' @param FN121 FN121 table that adheres to the FN2 data model
 #'
 #' @return
 #' @export
@@ -9,6 +9,8 @@
 #' \dontrun{
 #' fn121_column_check(FN121)
 #' }
+#'
+
 fn121_column_check <- function(FN121) {
   req_names <-
     c("PRJ_CD",
@@ -18,36 +20,40 @@ fn121_column_check <- function(FN121) {
       "EFFTM0",
       "EFFTM1",
       "SIDEP",
+      "SITEM",
       "EFFDUR")
   if (all(req_names %in% names(FN121))) {
     usethis::ui_done("FN121 has required fields")
   }
 
   if (!("PRJ_CD" %in% names(FN121))) {
-    usethis::ui_oops("PRJ_CD is required")
+    usethis::ui_oops("PRJ_CD is required in FN121")
   }
   if (!("SAM" %in% names(FN121))) {
-    usethis::ui_oops("SAM is required")
+    usethis::ui_oops("SAM is required in FN121")
   }
   if (!("EFFDT0" %in% names(FN121))) {
-    usethis::ui_oops("EFFDT0 is required")
+    usethis::ui_oops("EFFDT0 is required in FN121")
   }
   if (!("EFFDT1" %in% names(FN121))) {
-    usethis::ui_oops("EFFDT1 is required")
+    usethis::ui_oops("EFFDT1 is required in FN121")
   }
   if (!("EFFTM0" %in% names(FN121))) {
-    usethis::ui_oops("EFFTM0 is required")
+    usethis::ui_oops("EFFTM0 is required in FN121")
   }
   if (!("EFFTM1" %in% names(FN121))) {
-    usethis::ui_oops("EFFTM1 is required")
+    usethis::ui_oops("EFFTM1 is required in FN121")
   }
   if (!("SIDEP" %in% names(FN121))) {
-    usethis::ui_oops("SIDEP is required")
+    usethis::ui_oops("SIDEP is required in FN121")
+  }
+  if (!("SITEM" %in% names(FN121))) {
+    usethis::ui_oops("SITEM is required in FN121")
   }
   if (!("EFFDUR" %in% names(FN121))) {
-    usethis::ui_oops("EFFDUR is required")
+    usethis::ui_oops("EFFDUR is required in FN121")
   }
   if (!("EFFST" %in% names(FN121))) {
-    usethis::ui_oops("EFFST is required")
+    usethis::ui_oops("EFFST is required in FN121")
   }
 }
