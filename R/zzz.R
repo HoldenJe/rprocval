73870f8150454c71c8dbaa35eb32dac8979cc138
J. Holden <jeremy.holden@ontario.ca>
2022-04-29 14:51:37 -0400

    adds package version to start-up message

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
 .onAttach <- function(...) {
+  fn2message0 <- paste("rprocval version ", packageVersion('rprocval'))
   fn2message1 <- "Notice: functions in this package are designed to use FN2 variable names"
   fn2message2 <- "Data not using this model will need to be formatted to fit the FN2 naming convention"
-  fn2message <- paste(fn2message1, fn2message2, sep="\n")
+  fn2message <- paste(fn2message0, fn2message1, fn2message2, sep="\n")
   packageStartupMessage(fn2message)
 }
