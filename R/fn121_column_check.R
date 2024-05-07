f4b30da5bc6a5dd861216dcc5366fd5fbbe7cb04
J. Holden <jeremy.holden@ontario.ca>
2024-05-07 10:23:45 -0400

    modifies FN121 checks based on T5

--- R/fn121_column_check.R
+++ R/fn121_column_check.R
@@ -58,3 +58,65 @@ fn121_column_check <- function(FN121) {
     usethis::ui_oops("EFFST is required in FN121")
   }
 }
+
+
+#' Check FN121 Columns based on GL Template 5
+#'
+#' @param FN121 FN121 table that adheres to the GL Template 5 data model
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' fn121_column_check_t5(FN121)
+#' }
+#'
+
+fn121_column_check_t5 <- function(FN121) {
+  req_names <-
+    c("PRJ_CD",
+      "SAM",
+      "EFFDT0",
+      "EFFDT1",
+      "EFFTM0",
+      "EFFTM1",
+      "SIDEP0",
+      "SITEM0",
+      "EFFDUR",
+      "EFFST")
+  if (all(req_names %in% names(FN121))) {
+    usethis::ui_done("FN121 has required fields")
+  }
+
+  if (!("PRJ_CD" %in% names(FN121))) {
+    usethis::ui_oops("PRJ_CD is required in FN121")
+  }
+  if (!("SAM" %in% names(FN121))) {
+    usethis::ui_oops("SAM is required in FN121")
+  }
+  if (!("EFFDT0" %in% names(FN121))) {
+    usethis::ui_oops("EFFDT0 is required in FN121")
+  }
+  if (!("EFFDT1" %in% names(FN121))) {
+    usethis::ui_oops("EFFDT1 is required in FN121")
+  }
+  if (!("EFFTM0" %in% names(FN121))) {
+    usethis::ui_oops("EFFTM0 is required in FN121")
+  }
+  if (!("EFFTM1" %in% names(FN121))) {
+    usethis::ui_oops("EFFTM1 is required in FN121")
+  }
+  if (!("SIDEP0" %in% names(FN121))) {
+    usethis::ui_oops("SIDEP0 is required in FN121")
+  }
+  if (!("SITEM0" %in% names(FN121))) {
+    usethis::ui_oops("SITEM0 is required in FN121")
+  }
+  if (!("EFFDUR" %in% names(FN121))) {
+    usethis::ui_oops("EFFDUR is required in FN121")
+  }
+  if (!("EFFST" %in% names(FN121))) {
+    usethis::ui_oops("EFFST is required in FN121")
+  }
+}
