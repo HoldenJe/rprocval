cf43f8646a3e8d28fa9adfd0cb91e4e06705cab3
J. Holden <jeremy.holden@ontario.ca>
2024-05-07 12:16:07 -0400

    adds fultons test

---
+++ R/template5_tests.R
@@ -0,0 +1,38 @@
+# template 5 functions
+
+template5_tests <- function(FN012, FN022, FN026, FN026_Subspace, FN028, FN121, FN123, FN125) {}
+
+
+
+#' Check FN125 data against FN012 table
+#'
+#' @param FN125 FN125 table from Template 5
+#' @param FN012 FN012 table from Temaplate 5
+#' @param makeplot option to produce plot. Default = TRUE
+#'
+#' @return FN125 table with Fulton K, ranges from FN012 and categorization
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' FN125 <- fn125_fulton(FN125, FN012, makeplot = T)
+#' FN125 <- fn125_fulton(FN125, FN012, makeplot = F)
+#' }
+
+fn125_fulton <- function(FN125, FN012, makeplot = TRUE){
+  FN125 <- left_join(FN125, FN012, by = c("PRJ_CD", "SPC", "GRP")) %>%
+    mutate(FultonK = RWT/(TLEN^3)*100000) %>%
+    mutate(FultonCheck = case_when(
+      FultonK > K_MAX_ERROR | FultonK < K_MIN_ERROR ~"Exceeds_Max",
+      FultonK > K_MAX_WARN | FultonK < K_MIN_WARN ~"Exceeds_Warning",
+      .default = "Pass"
+    ))
+
+  p <- ggplot(FN125, aes(TLEN, RWT, color = FultonCheck)) +
+    geom_point() +
+    facet_wrap(~SPC, scales = "free") +
+    ggtitle(paste("Fulton's K. SPC", unique(FN125$SPC), sep = ":"))
+
+  if(makeplot) {print(p)}
+  return(FN125)
+}
